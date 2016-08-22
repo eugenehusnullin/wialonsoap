649a7809eb7c15b4bb8cd113f178cbb66be24152
echo <goalinternet@gmail.com>
2016-08-22 15:32:30 +0300

    log

--- src/main/java/ru/fors/udo/telemetry/webservice/TelemetryService.java
+++ src/main/java/ru/fors/udo/telemetry/webservice/TelemetryService.java
@@ -1,5 +1,7 @@
 package ru.fors.udo.telemetry.webservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -7,12 +9,14 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class TelemetryService {
+	private static final Logger logger = LoggerFactory.getLogger(TelemetryService.class);
 
 	private static final String NAMESPACE_URI = "http://webservice.telemetry.udo.fors.ru/";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "storeTelemetryList")
 	@ResponsePayload
 	public StoreTelemetryListResponse storeTelemetryList(@RequestPayload StoreTelemetryList list) {
+		logger.debug("enter storeTelemetryList");
 		return new StoreTelemetryListResponse();
 	}
 
